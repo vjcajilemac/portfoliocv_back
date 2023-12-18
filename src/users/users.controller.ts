4c513020f3c9f15444f48a192a0925c469bfa1b2
vjcajilemac <vjcajilema@gmail.com>
2023-12-18 01:04:12 -0500

    find user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,30 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersServices: UsersService) {}
 
   @Get()
-  getUsers() {
+  getUsers(): Promise<User[]> {
     return this.usersServices.getUsers();
+  }
+  @Get(':id')
+  getUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
+    return this.usersServices.getUser(id);
   }
 
   @Post()
-  createUser(@Body() newUser: CreateUserDTO) {
+  createUser(@Body() newUser: CreateUserDTO): Promise<User> {
     return this.usersServices.createUser(newUser);
   }
 }
